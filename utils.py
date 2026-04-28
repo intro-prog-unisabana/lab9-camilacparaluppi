fb763641557d7243c0a59d334cf0f7bb555f4886
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-28 18:07:48 -0500

    problema 5c

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
 # utils.py
+def person_data():
+    nombre = input("Enter the person's name:")
+    numerodecuenta = int(input("Enter a 4-digit account number:"))
+    saldoinicial = float(input("Enter the initial balance:"))
+person = ()
+account = BankAccount(numerodecuenta, saldoinicial)
+person.accounts.append(account)
+pregunta = input("Are you done adding accounts? (yes/no): ")
+if pregunta == "yes":
+    break

61bacb0eda0c8cb5b2b9737c54ed6ccc0302127f
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-28 17:49:06 -0500

    problema 5b

--- person.py
+++ person.py
@@ -1,1 +1,8 @@
 # person.py
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def add_account(self, account):
+        self.account = account
+    def __str__(self):
+        return f"Name = {self.name}, Number of accounts = {self.account}"

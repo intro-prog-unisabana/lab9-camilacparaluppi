342f03de82d48910821c1eaa33721c7b3cc66949
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-28 19:31:56 -0500

    problema 6

---
+++ aircraft_altitude.py
@@ -0,0 +1,15 @@
+from aircraft import Aircraft
+modeloaeronave = input("Enter aircraft model: ")
+plane = Aircraft(modeloaeronave)
+while True:
+    comando = input("Enter command (A for ascent, D for descent, X to exit): ")
+    if comando == "X":
+        break
+    partes = comando.split()  
+    direccion = partes[0]
+    pies = int(partes[1])
+    if direccion == "A":
+        plane.ascend(pies)
+    elif direccion == "D":
+        plane.descend(pies)
+print(f"Final altitude: {plane.altitude} feet")
